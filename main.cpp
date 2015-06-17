1c506db68ebf12dea983986d01e15836e8f6fdaa
Sergio Vieira <sergiosvieira@gmail.com>
2015-06-17 10:23:30 -0300

    creating zip file on linux

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <cstring>
+#include <vector>
+#include <iostream>
 
 struct mydata {
         const char *name;
         int fd;
 };
-
 
 int
 myopen(struct archive *a, void *client_data)
@@ -47,41 +48,40 @@ mywrite(struct archive *a, void *client_data, const void *buff, size_t n)
 }
 
 void
-write_archive(const char *outname, const char **filename)
+write_archive(const char *outname, const std::vector<std::string>& a_vector)
 {
-  struct mydata *mydata = (struct mydata*)malloc(sizeof(struct mydata));
-  struct archive *a;
-  struct archive_entry *entry;
-  struct stat st;
-  char buff[8192];
-  int len;
-  int fd;
-
-
-  a = archive_write_new();
-  mydata->name = outname;
-  archive_write_zip_set_compression_store(a);
-  archive_write_set_format_zip(a);
-  archive_write_open(a, mydata, myopen, mywrite, myclose);
-  while (*filename) {
-    stat(*filename, &st);
-    entry = archive_entry_new();
-    archive_entry_copy_stat(entry, &st);
-    archive_entry_set_pathname(entry, *filename);
-    archive_write_header(a, entry);
-    fd = open(*filename, O_RDONLY);
-    len = read(fd, buff, sizeof(buff));
-    while ( len > 0 ) {
-        archive_write_data(a, buff, len);
+    struct mydata *mydata = (struct mydata*)malloc(sizeof(struct mydata));
+    struct archive *a;
+    struct archive_entry *entry;
+    struct stat st;
+    char buff[8192];
+    int len;
+    int fd;
+    a = archive_write_new();
+    mydata->name = outname;
+    archive_write_zip_set_compression_store(a);
+    archive_write_set_format_zip(a);
+    archive_write_open(a, mydata, myopen, mywrite, myclose);
+    for (const std::string& file: a_vector)
+    {
+        stat(file.c_str(), &st);
+        entry = archive_entry_new();
+        archive_entry_copy_stat(entry, &st);
+        archive_entry_set_pathname(entry, file.c_str());
+        archive_write_header(a, entry);
+        fd = open(file.c_str(), O_RDONLY);
         len = read(fd, buff, sizeof(buff));
+        while (len > 0)
+        {
+            archive_write_data(a, buff, len);
+            len = read(fd, buff, sizeof(buff));
+        }
+        archive_entry_free(entry);
     }
-    archive_entry_free(entry);
-    filename++;
-  }
-  archive_write_finish(a);
+    archive_write_finish(a);
 }
 
-void listDir(DIR* a_parent, const char* a_dirName, int a_level)
+void listDir(std::vector<std::string>& a_vector, DIR* a_parent, const char* a_dirName, int a_level)
 {
     DIR* dir = nullptr;
     dir = opendir(a_dirName);
@@ -99,7 +99,6 @@ void listDir(DIR* a_parent, const char* a_dirName, int a_level)
                     {
                         printf("  ");
                     }
-                    printf("[%s]\n", entity->d_name);
                     char fullName[256] = {};
                     if (a_parent != nullptr)
                     {
@@ -112,7 +111,9 @@ void listDir(DIR* a_parent, const char* a_dirName, int a_level)
                     }
                     strcat(fullName, "/");
                     strcat(fullName, entity->d_name);
-                    listDir(dir, fullName, a_level + 1);
+                    //printf("[%s]\n", fullName);
+                    listDir(a_vector, dir, fullName, a_level + 1);
+                    a_vector.push_back(fullName);
                 }
             }
             else
@@ -121,7 +122,12 @@ void listDir(DIR* a_parent, const char* a_dirName, int a_level)
                 {
                     printf("  ");
                 }
-                printf("%s\n", entity->d_name);
+                char fullName[256] = {};
+                strcpy(fullName, a_dirName);
+                strcat(fullName, "/");
+                strcat(fullName, entity->d_name);
+                //printf("%s\n", fullName);
+                a_vector.push_back(fullName);
             }
         }
     }
@@ -129,11 +135,15 @@ void listDir(DIR* a_parent, const char* a_dirName, int a_level)
 
 int main(int argc, const char **argv)
 {
+    std::vector<std::string> files;
     if (argv[1] != nullptr)
     {
-        printf("[%s]\n", argv[1]);
-        listDir(nullptr, argv[1], 1);
+        listDir(files, nullptr, argv[1], 1);
     }
-    //write_archive("saida.zip", argv);
+    for (const std::string& file: files)
+    {
+        std::cout << file << "\n";
+    }
+    write_archive("saida.zip", files);
     return 0;
 }
